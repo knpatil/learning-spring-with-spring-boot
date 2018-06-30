f169eb45f9f3055709fd5c9d6e3abfb4ec5577bb
Kamlakar Patil <kpatil@salesforce.com>
2018-06-30 15:37:09 -0700

    first commit

---
+++ src/main/java/com/kpatil/newyork/data/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+package com.kpatil.newyork.data.repository;
+
+import com.kpatil.newyork.data.entity.Guest;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends PagingAndSortingRepository<Guest, Long> {
+
+}
